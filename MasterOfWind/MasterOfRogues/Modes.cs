d2e5db9ba37c18dfc17e03e442ca6912b47533cf
sergi martinex vaya <sergi_vaya@hotmail.com>
2015-05-10 04:38:27 +0200

    Master Of Wind
    new yasuo :dancer: suo :D

---
+++ MasterOfWind/MasterOfRogues/Modes.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+namespace MasterOfWind
+{
+    class Modes
+    {
+        public Obj_AI_Base selectedminion;
+        public Obj_AI_Base selectedminions;
+        private Obj_AI_Hero target;
+       private Program p;
+      private  Skills skills;
+        public Skills getSkills()
+      {
+          return skills;
+      }
+        public void load(Program p)
+        {
+            skills = new Skills();
+            this.p = p;
+            target = TargetSelector.GetTarget(1500, TargetSelector.DamageType.Magical);
+        }
+        public Obj_AI_Hero getTarget()
+        {
+            target = TargetSelector.GetTarget(1500, TargetSelector.DamageType.Magical);
+            return target;
+        }
+        //combos
+        public void laneClear()
+        {
+
+            var minion = MinionManager.GetMinions(skills.getE().Range, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.MaxHealth).FindAll(m => !m.HasBuff("YasuoDashWrapper")).FirstOrDefault();
+            var useQ = p.getMenu().Item("QL").GetValue<bool>();
+            var useE = p.getMenu().Item("EL").GetValue<bool>();
+            if (useQ &&  !useE) skills.qCast(minion);
+            else if (!useQ &&  useE) skills.eCast(minion);
+            else if (!useQ && useE)  
+            {
+                skills.eCast(minion);
+            }
+            
+            else if (useQ &&  useE)
+            {
+                skills.qCast(minion);
+                skills.eCast(minion);
+            }
+            else if (useQ &&  !useE)
+            {
+                skills.qCast(minion);
+            }
+            else if (useQ &&  useE)
+            {
+                skills.qCast(minion);
+                skills.eCast(minion);
+            }
+            else
+                return;
+            selectedminion = minion;
+        }
+        public void jungleClear()
+        {
+
+            var minion = MinionManager.GetMinions(skills.getE().Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth).FindAll(m => !m.HasBuff("YasuoDashWrapper")).FirstOrDefault();
+            var useQ = p.getMenu().Item("QJ").GetValue<bool>();
+            var useE = p.getMenu().Item("EJ").GetValue<bool>();
+            if (useQ && !useE) skills.qCast(minion);
+            else if (!useQ &&  useE) skills.eCast(minion);
+            else if (!useQ &&  useE)
+            {
+                skills.eCast(minion);
+            }
+
+            else if (useQ &&  useE)
+            {
+                skills.qCast(minion);
+                skills.eCast(minion);
+            }
+            else if (useQ &&  !useE)
+            {
+                skills.qCast(minion);
+            }
+            else if (useQ &&  useE)
+            {
+                skills.qCast(minion);
+                skills.eCast(minion);
+            }
+            else
+                return;
+            selectedminion = minion;
+        }
+        public void harrash(Obj_AI_Hero target)
+        {
+           //    skills.qCast(target);
+            var useQ = p.getMenu().Item("QH").GetValue<bool>();
+            var useE = p.getMenu().Item("EH").GetValue<bool>();
+    //        int q = p.getMenu().Item("sethQ").GetValue<Slider>().Value;
+            if (useQ &&  !useE)
+            {
+                skills.qCast(target);
+            }
+            else if (!useQ && useE)
+            {
+                skills.eCast(target);
+            }
+            else if (useQ &&  useE)
+            {
+                skills.qCast(target);
+                skills.eCast(target);
+            }
+            else if (useQ && !useE)
+            {
+                skills.qCast(target);
+            }
+            else if (useQ &&  useE) //   Q+W+Q+E+Q+R
+            {
+                skills.eCast(target);
+                skills.qCast(target);
+            }
+            else
+            {
+                return;
+            }
+        }
+        public void flee(Obj_AI_Base target)
+        {
+            //use w for flee?
+            //load second q for flee?
+          p.getPlayer().IssueOrder(GameObjectOrder.MoveTo, p.getPlayer().Position.Extend(Game.CursorPos, 150));
+            eFlee();
+        }
+        public void eFlee()
+        {
+            Obj_AI_Base minion = ObjectManager.Get<Obj_AI_Base>().Where(x => x.IsMinion && skills.getE().IsInRange(x) && !x.HasBuff("YasuoDashWrapper")).MinOrDefault(x => x.Distance(Game.CursorPos));
+            if (minion.Distance(Game.CursorPos) < ObjectManager.Player.Distance(Game.CursorPos))
+           {
+                skills.eCast(minion);
+                selectedminions = minion;
+           }
+       }
+        public void eLogic(Obj_AI_Hero Target)
+        {
+            if(!skills.getE().IsInRange(Target))
+            {
+                Obj_AI_Base minion = ObjectManager.Get<Obj_AI_Base>().Where(x => x.IsMinion && skills.getE().IsInRange(x) && !x.HasBuff("YasuoDashWrapper")).MinOrDefault(x => x.Distance(Target));
+                if (minion.Distance(target) < ObjectManager.Player.Distance(target))
+                {
+                    skills.eCast(minion);
+                    selectedminions = minion;
+                }
+            }
+            else
+            {
+                skills.eCast(Target);
+                selectedminions = Target;
+            }
+        }
+             
+        public void combo(Obj_AI_Hero target)
+        {
+           var useQ = p.getMenu().Item("QC").GetValue<bool>();
+            var useE = p.getMenu().Item("EC").GetValue<bool>();
+            var useR = p.getMenu().Item("RC").GetValue<bool>();
+            if (useQ &&  !useE) skills.qCast(target);
+            else if (!useQ && useE) eLogic(target);
+            else if (!useQ &&  useE)
+            {
+                eLogic(target);
+            }
+            else if (useQ && useE)
+            {
+                skills.qCast(target);
+                eLogic(target);
+            }
+            else if (useQ && !useE)
+            {
+                skills.qCast(target);
+            }
+            else if (useQ &&  useE) //   Q+W+Q+E+Q+R
+            {
+                skills.qCast(target);
+                eLogic(target);
+            }
+            else
+                return;
+            if (useR)
+                skills.rCast(target);
+        }
+
+    }
+}
---
+++ MasterOfWind/MasterOfRogues/Skills.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace MasterOfWind
+{
+    class Skills 
+    {
+        private Spell Q,SecondQ, W, E, R;
+        private SpellSlot ignite;
+
+        public Skills()
+        {
+            Q = new Spell(SpellSlot.Q, 475); // line
+            SecondQ = new Spell(SpellSlot.Q, 1000);
+            W = new Spell(SpellSlot.W, 400);  // wall
+            E = new Spell(SpellSlot.E, 475); // line
+            R = new Spell(SpellSlot.R,1200); // circle
+            Q.SetSkillshot(Q.Instance.SData.SpellCastTime, Q.Instance.SData.LineWidth, Q.Instance.SData.MissileSpeed, true, SkillshotType.SkillshotLine);
+            SecondQ.SetSkillshot(Q.Instance.SData.SpellCastTime, Q.Instance.SData.LineWidth, Q.Instance.SData.MissileSpeed, true, SkillshotType.SkillshotLine);
+            ignite = ObjectManager.Player.GetSpellSlot("SummonerDot");
+        }
+
+       public Spell getQ()
+        {
+            return Q;
+        }
+       public Spell getW()
+       {
+           return W;
+       }
+       public Spell getE()
+       {
+           return E;
+       }
+       public Spell getR()
+       {
+           return R;
+       }
+       public HitChance HitchanceCheck(int i)
+       {
+           switch (i)
+           {
+               case 1:
+                   return HitChance.Low;
+               case 2:
+                   return HitChance.Medium;
+               case 3:
+                   return HitChance.High;
+               case 4:
+                   return HitChance.VeryHigh;
+           }
+           return HitChance.Low;
+       }
+       public bool qCast(Obj_AI_Base target)
+       {
+           if (target == null) return false;
+           if (!HaveQ3)
+           {
+               if (Q.IsReady())
+               {
+                   Q.CastIfHitchanceEquals(target, HitChance.High);
+                   return true;
+               }
+           }
+           else
+           {
+               if (SecondQ.IsReady())
+               {
+                   SecondQ.CastIfHitchanceEquals(target, HitChance.High);
+                   return true;
+               }
+           }
+           return false;
+
+       }
+        public bool wCast(Obj_AI_Base  target)
+       {
+           if (target == null) return false;
+      //wall not coded
+           return false;
+       }
+        public bool eCast(Obj_AI_Base  target)
+        {
+                      if (target == null) return false;
+                      if (E.IsReady() && E.IsInRange(target))
+                      {
+                          E.Cast(target);
+                          return true;
+                      }
+             return false;
+        }
+        private bool canCastR(Obj_AI_Base target)
+        {
+            return target.HasBuffOfType(BuffType.Knockup) || target.HasBuffOfType(BuffType.Knockback);
+        }
+        public  bool rCast(Obj_AI_Base  target)
+        {
+           if (target == null) return false;
+           if (R.IsReady() && canCastR(target))
+           {
+               R.Cast();
+               return true;
+           }
+           return false;
+        }
+        public bool HaveQ3
+        {
+            get { return ObjectManager.Player.HasBuff("YasuoQ3W"); }
+        }
+        public bool IgniteCast(Obj_AI_Hero target)
+        {
+            if (ignite.IsReady() && target.Health - ObjectManager.Player.GetSummonerSpellDamage(target, Damage.SummonerSpell.Ignite) <= 0)
+            {
+                ObjectManager.Player.Spellbook.CastSpell(ignite, target);
+                return true;
+            }
+            return false;
+        }
+    }
+}
