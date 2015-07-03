c7253e04720d1955099f0bc3cb3c26b42c5aa72b
sergi martinex vaya <sergi_vaya@hotmail.com>
2015-07-03 15:46:16 +0200

    Master of jumps
    Master of jumps ;)

---
+++ MasterOfJumps/MasterOfJumps/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace MasterOfJumps
+{
+    enum ChampName
+    {
+        LeeSin = 0,
+        Jax = 1,
+        Katarina = 2
+    }
+    class Program
+    {
+        //KATARINA LEE JAX 
+        public static Menu menu;
+        public static ChampName name;
+        public static Spell QJax, WLee, EKata;
+        public static Obj_AI_Hero Player;
+        public static Vector3 wardPosition;
+        public static bool jumped;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
+        }
+
+        private static void OnGameLoad(EventArgs args)
+        {
+             Player = ObjectManager.Player;
+            if (Player.ChampionName == "Lee Sin")
+            {
+                WLee = new Spell(SpellSlot.W, 700);
+                name = ChampName.LeeSin;
+            }
+
+            else if (Player.ChampionName == "Jax")
+            {
+                QJax = new Spell(SpellSlot.Q, 700);
+                name = ChampName.Jax;
+            }
+            else if (Player.ChampionName == "Katarina")
+            {
+                EKata = new Spell(SpellSlot.W, 700);
+                name = ChampName.Katarina;
+            }
+            Menu();
+            Game.OnUpdate += Game_OnGameUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (menu.Item("Drawings").IsActive())
+            {
+                if (ChampName.Jax == name)
+                {
+                    Drawing.DrawCircle(Player.Position, QJax.Range, System.Drawing.Color.Green);
+                }
+                else if (ChampName.LeeSin == name)
+                {
+                    Drawing.DrawCircle(Player.Position, WLee.Range, System.Drawing.Color.Green);
+                }
+                else if (ChampName.Katarina == name)
+                {
+                    Drawing.DrawCircle(Player.Position, EKata.Range, System.Drawing.Color.Green);
+                }
+            }
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (menu.Item("wardjump").GetValue<KeyBind>().Active)
+            {
+                if (ChampName.Jax == name)
+                {
+                    WardJumpJax();
+                }
+                else if (ChampName.LeeSin == name)
+                {
+                    WardJumpLee();
+                }
+                else if (ChampName.Katarina == name)
+                {
+                    WardJumpKata();
+                }
+            }
+        }
+
+        public static void Menu()
+        {
+            menu = new Menu("Master Of Jump", "MasterOfJump", true);
+            menu.AddItem(new MenuItem("Drawings", "draws").SetValue(true));
+            menu.AddItem(new MenuItem("wardjump", "WardJump key").SetValue(new KeyBind('Z', KeyBindType.Press)));
+            menu.AddToMainMenu();
+        }
+        public static void WardJumpJax()
+        {
+            Player.IssueOrder(GameObjectOrder.MoveTo, Player.Position.Extend(Game.CursorPos, 150));
+
+            if (QJax.IsReady())
+            {
+                wardPosition = Game.CursorPos;
+                Obj_AI_Minion Wards;
+                if (Game.CursorPos.Distance(Program.Player.Position) <= 700)
+                {
+                    Wards = ObjectManager.Get<Obj_AI_Minion>().Where(ward => ward.Distance(Game.CursorPos) < 150 && !ward.IsDead).FirstOrDefault();
+                }
+                else
+                {
+                    Vector3 cursorPos = Game.CursorPos;
+                    Vector3 myPos = Player.ServerPosition;
+                    Vector3 delta = cursorPos - myPos;
+                    delta.Normalize();
+                    wardPosition = myPos + delta * (600 - 5);
+                    Wards = ObjectManager.Get<Obj_AI_Minion>().Where(ward => ward.Distance(wardPosition) < 150 && !ward.IsDead).FirstOrDefault();
+                }
+                if (Wards == null)
+                {
+                    if (!wardPosition.IsWall())
+                    {
+                        InventorySlot invSlot = Items.GetWardSlot();
+                        Items.UseItem((int)invSlot.Id, wardPosition);
+                        jumped = true;
+                    }
+                }
+
+                else
+                    if (QJax.CastOnUnit(Wards))
+                    {
+                        jumped = false;
+                    }
+            }
+        }
+        public static void WardJumpLee()
+        {
+            Player.IssueOrder(GameObjectOrder.MoveTo, Player.Position.Extend(Game.CursorPos, 150));
+
+            if (WLee.IsReady() && ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name == "BlindMonkWOne")
+            {
+                wardPosition = Game.CursorPos;
+                Obj_AI_Minion Wards;
+                if (Game.CursorPos.Distance(Program.Player.Position) <= 700)
+                {
+                    Wards = ObjectManager.Get<Obj_AI_Minion>().Where(ward => ward.Distance(Game.CursorPos) < 150 && !ward.IsDead).FirstOrDefault();
+                }
+                else
+                {
+                    Vector3 cursorPos = Game.CursorPos;
+                    Vector3 myPos = Player.ServerPosition;
+                    Vector3 delta = cursorPos - myPos;
+                    delta.Normalize();
+                    wardPosition = myPos + delta * (600 - 5);
+                    Wards = ObjectManager.Get<Obj_AI_Minion>().Where(ward => ward.Distance(wardPosition) < 150 && !ward.IsDead).FirstOrDefault();
+                }
+                if (Wards == null)
+                {
+                    if (!wardPosition.IsWall())
+                    {
+                        InventorySlot invSlot = Items.GetWardSlot();
+                        Items.UseItem((int)invSlot.Id, wardPosition);
+                        jumped = true;
+                    }
+                }
+
+                else
+                    if (Program.WLee.CastOnUnit(Wards))
+                    {
+                        jumped = false;
+                    }
+            }
+
+        }
+        public static void WardJumpKata()
+        {
+            Player.IssueOrder(GameObjectOrder.MoveTo, Player.Position.Extend(Game.CursorPos, 150));
+            if (EKata.IsReady())
+            {
+                wardPosition = Game.CursorPos;
+                Obj_AI_Minion Wards;
+                if (Game.CursorPos.Distance(Program.Player.Position) <= 700)
+                {
+                    Wards = ObjectManager.Get<Obj_AI_Minion>().Where(ward => ward.Distance(Game.CursorPos) < 150 && !ward.IsDead).FirstOrDefault();
+                }
+                else
+                {
+                    Vector3 cursorPos = Game.CursorPos;
+                    Vector3 myPos = Player.ServerPosition;
+                    Vector3 delta = cursorPos - myPos;
+                    delta.Normalize();
+                    wardPosition = myPos + delta * (600 - 5);
+                    Wards = ObjectManager.Get<Obj_AI_Minion>().Where(ward => ward.Distance(wardPosition) < 150 && !ward.IsDead).FirstOrDefault();
+                }
+                if (Wards == null)
+                {
+                    if (!wardPosition.IsWall())
+                    {
+                        InventorySlot invSlot = Items.GetWardSlot();
+                        Items.UseItem((int)invSlot.Id, wardPosition);
+                        jumped = true;
+                    }
+                }
+
+                else
+                    if (EKata.CastOnUnit(Wards))
+                    {
+                        jumped = false;
+                    }
+            }
+        }
+    }
+}
---
+++ MasterOfJumps/MasterOfJumps/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MasterOfJumps")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MasterOfJumps")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("12abac41-1854-4298-a7d1-f689c5ce5cfa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
